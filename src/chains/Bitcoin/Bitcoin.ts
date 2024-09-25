ea72af3404979970173e6870351d03ef7c04104b
Pessina <fs.pessina@gmail.com>
2024-09-25 15:09:35 +0700

    Move types to types file

--- src/chains/Bitcoin/Bitcoin.ts
+++ src/chains/Bitcoin/Bitcoin.ts
@@ -15,47 +15,8 @@ import {
   type BTCTransaction,
   type UTXO,
   type BTCOutput,
+  type Transaction,
 } from './types'
-
-interface Transaction {
-  txid: string
-  version: number
-  locktime: number
-  size: number
-  weight: number
-  fee: number
-  vin: Array<{
-    txid: string
-    vout: number
-    is_coinbase: boolean
-    scriptsig: string
-    scriptsig_asm: string
-    inner_redeemscript_asm: string
-    inner_witnessscript_asm: string
-    sequence: number
-    witness: string[]
-    prevout: any
-    is_pegin: boolean
-    issuance: any
-  }>
-  vout: Array<{
-    scriptpubkey: string
-    scriptpubkey_asm: string
-    scriptpubkey_type: string
-    scriptpubkey_address: string
-    value: number
-    valuecommitment: string
-    asset: string
-    assetcommitment: string
-    pegout: any
-  }>
-  status: {
-    confirmed: boolean
-    block_height: number | null
-    block_hash: string | null
-    block_time: number | null
-  }
-}
 
 export class Bitcoin {
   private readonly network: BTCNetworkIds
@@ -132,8 +93,7 @@ export class Bitcoin {
 
     data.vin.forEach((vin) => {
       const txHash = Buffer.from(vin.txid, 'hex').reverse()
-      const { vout } = vin
-      const { sequence } = vin
+      const { vout, sequence } = vin
       const scriptSig = vin.scriptsig
         ? Buffer.from(vin.scriptsig, 'hex')
         : undefined
