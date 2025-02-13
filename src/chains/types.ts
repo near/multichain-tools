fd456835e46c97c9f14d6d51f9a6b1e6fba3b072
Peter <petru.papucciu@nearone.org>
2025-02-13 17:45:09 +0000

    MPC removed unsupported contract from comments

--- src/chains/types.ts
+++ src/chains/types.ts
@@ -2,7 +2,6 @@
 Available ChainSignature contracts:
   - Mainnet: v1.signer
   - Testnet: v1.signer-prod.testnet
-  - Development (unstable): v1.signer-dev.testnet
 */
 export type ChainSignatureContracts = string
 
